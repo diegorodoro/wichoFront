5011d8e3fbc1930248629dc7ae288290d2ea6424
inigo1405 <quintanadinigo@gmail.com>
2025-05-13 19:11:21 -0600

    Add Tailwind CSS integration and refactor styles; remove unused App.css

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Main: React.FC = () => {
     return (
         <div>
-            <h1>Main Component</h1>
+            <h1 className="text-blue-600">Main Component</h1>
         </div>
     );
 };
